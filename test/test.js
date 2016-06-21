476f1f8d3ed79ace58572e38559923f2741dfe4c
Ruben Piatnitsky <rub3@pdx.edu>
2016-06-21 16:53:44 -0700

    Mocha npm install and added sample code

---
+++ test/test.js
@@ -0,0 +1,10 @@
+// Mocha test example code
+var assert = require('chai').assert;
+
+describe('Array', function () {
+  describe('indexOf()', function () {
+    it('should return 8 when value is empty', function () {
+      assert.equal(-1, [1,2,3].indexof(5);
+    });
+  });
+});
