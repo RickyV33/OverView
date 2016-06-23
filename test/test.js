f76b995d412af877f1b0b39ad087346d4eb03adf
Ruben <rub3@pdx.edu>
2016-06-23 16:40:50 -0700

    Updated chai and changed the testfile

--- test/test.js
+++ test/test.js
@@ -1,10 +1,29 @@
 // Mocha test example code
-var assert = require('chai').assert;
+//var chaijq = require('chai-jquery');
 
-describe('Array', function () {
-  describe('indexOf()', function () {
-    it('should return 8 when value is empty', function () {
-      assert.equal(-1, [1,2,3].indexof(5);
+var chai = require('chai');
+var assert = chai.assert;
+var should = chai.should;
+var expect = chai.expect;
+var chaiHttp = require('chai-http');
+var server = require('../app.js');
+
+// http://mherman.org/blog/2015/09/10/testing-node-js-with-mocha-and-chai/
+chai.use(chaiHttp);
+//chai.use(chaijq);
+
+
+describe('Index Page', function () {
+    it('should contain a property called text', function(done) {
+        var ti = 'Jama Software Capstone';
+
+        chai.request(server)
+            .get('/')
+            .end(function(err, res){
+                console.log(res.body);
+                expect(res).to.have.property('text');
+                done();
+            });
     });
-  });
 });
+
