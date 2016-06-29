1c9e9baf9e2866d3883cdd0cafbbe12ef57478c2
Ricky Valencia <3d.rickyV@gmail.com>
2016-06-28 19:47:20 -0700

    start server side authentication and validation

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var auth = require('../lib/auth');
 var router = express.Router();
 
 /* GET home page. */
@@ -6,5 +7,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Jama Software Capstone' });
 });
 
+router.post('/', function(req, res, next) {
+  if (auth.validate(req)) {
+    auth.authenticate(req.body.username, req.body.password);
+    res.render('index', { title: 'Welcome to JamaTrace.' });
+  } else {
+    res.render('index', { title: 'Error: Incorrect username and/or password, please try again.' });
+  }
+
+});
 
 module.exports = router;
--- src/index.js
+++ src/index.js
@@ -2,19 +2,19 @@
 const INVALID_USERNAME_PASSWORD_ERROR_MESSAGE = "Invalid Username and/or Password Combination";
 
 document.addEventListener('DOMContentLoaded', () => {
-    document.getElementById("logIn").addEventListener('submit', (event) => {
-        let username = document.getElementById("username").value;
-        let password = document.getElementById("password").value;
-        if (username && password.length > 5 && username.length < 200 && password.length < 200) {
-            //run submit function
-        }
-        else{
-            event.preventDefault();
-            alert(INVALID_USERNAME_PASSWORD_ERROR_MESSAGE);
-            document.getElementById("username").value = "";
-            document.getElementById("password").value = "";
-        }
+  document.getElementById("logIn").addEventListener('submit', (event) => {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (username && password.length > 5 && username.length < 200 && password.length < 200) {
+      //run submit function
+    }
+    else{
+      event.preventDefault();
+      alert(INVALID_USERNAME_PASSWORD_ERROR_MESSAGE);
+      document.getElementById("username").value = "";
+      document.getElementById("password").value = "";
+    }
 
-    });
+  });
 
 });
