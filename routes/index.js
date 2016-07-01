3fdb1cce5bdf723db74b678df182195c0a466b36
Ricky Valencia <3d.rickyV@gmail.com>
2016-06-30 20:51:29 -0700

    Add teamName to session, etc.
    Make authenticate work (yay promises)
    Handle auth error in view render

--- routes/index.js
+++ routes/index.js
@@ -3,18 +3,24 @@ var auth = require('../lib/auth');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Jama Software Capstone' });
 });
 
-router.post('/', function(req, res, next) {
+router.post('/', function (req, res, next) {
   if (auth.validate(req)) {
-    auth.authenticate(req.body.username, req.body.password);
-    res.render('index', { title: 'Welcome to JamaTrace.' });
+    auth.authenticate(req.body.username, req.body.password, req.body.teamName).then(function (projects) {
+      req.session.username = req.body.username;
+      req.session.password = req.body.password;
+      req.session.teamName = req.body.teamName;
+      req.session.projects = projects;
+      res.redirect('/projects');
+    }, function () {
+      res.render('index', { title: 'Error: Incorrect credentials, please try again.' });
+    });
   } else {
-    res.render('index', { title: 'Error: Incorrect username and/or password, please try again.' });
+    res.render('index', { title: 'Error: Incorrect credentials, please try again.' });
   }
-
 });
 
 module.exports = router;
