78fa9454a592c8f2ec12e4d2449b6ce262c752ea
Ricky Valencia <3d.rickyV@gmail.com>
2016-06-30 22:38:24 -0700

    Lots of changes
    Add all test cases for credential validation
    Fix pagination module
    Fetch a given range of payload items
    Begin adding authenticate unit test

--- test/lib/auth.js
+++ test/lib/auth.js
@@ -3,12 +3,57 @@
 var chai = require('chai');
 var expect = chai.expect;
 var dirtyChai = require('dirty-chai');
+var 
 
 var auth = require('../../lib/auth');
 
 chai.use(dirtyChai);
 
 describe('Auth module', function () {
+  'use strict';
+  let credentialFixtureCases = [
+    {
+      username: '',
+      password: '',
+      teamName: ''
+    },
+    {
+      username: 'dummy',
+      password: '',
+      teamName: ''
+    },
+    {
+      username: '',
+      password: 'dumber',
+      teamName: ''
+    },
+    {
+      username: '',
+      password: '',
+      teamName: 'dummy'
+    },
+    {
+      username: 'dummy',
+      password: 'password',
+      teamName: ''
+    },
+    {
+      username: '',
+      password: 'password',
+      teamName: 'sevensource'
+    },
+    {
+      username: 'dummy',
+      password: '',
+      teamName: 'sevensource'
+    },
+    {
+      username: 'dummy',
+      password: 'password',
+      teamName: 'sevensource'
+    }
+  ];
+  
   describe('validate function', function () {
     'use strict';
     let credentialsFixture = {
@@ -21,39 +66,74 @@ describe('Auth module', function () {
       };
     });
 
-    it('should return false when all required fields are empty', function () {
-      credentialsFixture.body.username = '';
-      credentialsFixture.body.password = '';
-      credentialsFixture.body.teamName = '';
-      expect(auth.validate(credentialsFixture)).to.be.false();
+    credentialFixtureCases.forEach(function (fixture) {
+      let baseMessage = 'should return false when a username is ';
+      let username = fixture.username ? 'valid' : 'empty';
+      let password = fixture.password ? 'valid' : 'empty';
+      let teamName = fixture.teamName ? 'valid' : 'empty';
+
+      if (fixture.username && fixture.password && fixture.teamName) {
+        it('should return true when all required fields are valid', function () {
+          credentialsFixture.body = fixture;
+          expect(auth.validate(credentialsFixture)).to.be.true();
+        });
+      } else if (!fixture.username && !fixture.password && !fixture.teamName) {
+        it('should return false when all required fields are empty', function () {
+          credentialsFixture.body = fixture;
+          expect(auth.validate(credentialsFixture)).to.be.false();
+        });
+      } else {
+        it(baseMessage + username + ', password is ' + password + ', and team name is ' + teamName, function () {
+          credentialsFixture.body = fixture;
+          expect(auth.validate(credentialsFixture)).to.be.false();
+        });
+      }
     });
 
-    it('should return false when username is valid, password is empty, and team name is empty ', function () {
-      credentialsFixture.body.username = 'dummy';
-      credentialsFixture.body.password = '';
-      credentialsFixture.body.teamName = '';
-      expect(auth.validate(credentialsFixture)).to.be.false();
+    function stringOfLengthN (length) {
+      'use strict';
+      let string = '';
+      for (let i = 0; i < length; i += 1) {
+        string += 'a';
+      }
+      return string;
+    }
+
+    it('should return false when username length is more than 200', function () {
+      credentialFixtureCases.filter(function (fixture) {
+        return fixture.username;
+      }).forEach(function (fixture) {
+        fixture.username = stringOfLengthN(201);
+        credentialsFixture.body = fixture;
+
+        expect(auth.validate(credentialsFixture)).to.be.false();
+      });
     });
 
-    it('should return false when password is valid, username is empty, and team name is empty', function () {
-      credentialsFixture.body.username = '';
-      credentialsFixture.body.password = 'dummyy';
-      credentialsFixture.body.teamName = '';
-      expect(auth.validate(credentialsFixture)).to.be.false();
+    it('should return false when password length is more than 200', function () {
+      credentialFixtureCases.filter(function (fixture) {
+        return fixture.password;
+      }).forEach(function (fixture) {
+        fixture.password = stringOfLengthN(201);
+        credentialsFixture.body = fixture;
+
+        expect(auth.validate(credentialsFixture)).to.be.false();
+      });
     });
 
-    it('should return false when team name is valid, username is empty, and password is empty', function () {
-      credentialsFixture.body.username = '';
-      credentialsFixture.body.password = '';
-      credentialsFixture.body.teamName = 'dummy';
-      expect(auth.validate(credentialsFixture)).to.be.false();
-    });
+    it('should return false when password length is less than 6', function () {
+      credentialFixtureCases.filter(function (fixture) {
+        return fixture.password;
+      }).forEach(function (fixture) {
+        fixture.password = stringOfLengthN(5);
+        credentialsFixture.body = fixture;
 
-    it('should return false when username is valid, password is valid, and team name is empty', function () {
-      credentialsFixture.body.username = 'dummy';
-      credentialsFixture.body.password = 'password';
-      credentialsFixture.body.teamName = '';
-      expect(auth.validate(credentialsFixture)).to.be.false();
+        expect(auth.validate(credentialsFixture)).to.be.false();
+      });
     });
+  });
+
+  describe('authenticate function', function () {
+
   });
 });
