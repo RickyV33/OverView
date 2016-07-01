6f6298cfbbfe8187de3b5cece4db163e075c8a2d
Ricky Valencia <3d.rickyV@gmail.com>
2016-06-30 20:46:54 -0700

    Fix some linting errors

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var path = require('path');
-var favicon = require('serve-favicon');
 var logger = require('morgan');
 var session = require('express-session');
 var SQLiteStore = require('connect-sqlite3')(session);
@@ -14,13 +13,11 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(session({
-  store: new SQLiteStore,
+  store: new SQLiteStore(),
   secret: process.env.SESSION_SECRET || 'reallyBadSecret',
   cookie: { maxAge: 24 * 60 * 60 * 1000 }, // 1 day
   resave: false,
@@ -31,7 +28,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   var err = new Error('Not Found');
   err.status = 404;
   next(err);
@@ -42,7 +39,7 @@ app.use(function(req, res, next) {
 // development error handler
 // will print stacktrace
 if (app.get('env') === 'development') {
-  app.use(function(err, req, res, next) {
+  app.use(function (err, req, res, next) {
     res.status(err.status || 500);
     res.render('error', {
       message: err.message,
@@ -53,13 +50,12 @@ if (app.get('env') === 'development') {
 
 // production error handler
 // no stacktraces leaked to user
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   res.status(err.status || 500);
   res.render('error', {
     message: err.message,
     error: {}
   });
 });
-
 
 module.exports = app;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,9 @@
+/* eslint-env mocha */
 // Mocha test example code
 // To use the script, make sure to --> npm install -g mocha
 // To run tests, run command 'mocha test' or 'mocha <your script name>'
 
 var chai = require('chai');
-var assert = chai.assert;
 var should = chai.should;
 var expect = chai.expect;
 var chaiHttp = require('chai-http');
@@ -12,15 +12,14 @@ var server = require('../app.js');
 chai.use(chaiHttp);
 
 describe('Index Page', function () {
-    it('should contain a property called text', function(done) {
-
-        // This makes a server request to the route location '/'
-        chai.request(server)
-            .get('/')
-            .end(function(err, res){
-                expect(res).to.have.property('text');
-                done();
-            });
+  it('should contain a property called text', function (done) {
+    // This makes a server request to the route location '/'
+    chai.request(server).get('/').end(function (err, res) {
+      if (err) {
+        should.fail('Index Page Failed.');
+      }
+      expect(res).to.have.property('text');
+      done();
     });
+  });
 });
-
