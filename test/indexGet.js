7bcf2a79e62b91c1e054a042dfb4629fac1ae101
Ruben Piatnitsky <rub3@pdx.edu>
2016-07-06 15:38:08 -0700

    Added a test to verify that the response HTML is the same as EJS generated HTML

--- test/indexGet.js
+++ test/indexGet.js
@@ -4,6 +4,10 @@ let chai = require('chai');
 let chaiHttp = require('chai-http');
 let server = require('../app');
 let expect = chai.expect;
+// For Rendering the html using ejs
+let ejs = require('ejs');
+let read = require('fs').readFileSync;
+let join = require('path').join;
 
 chai.use(chaiHttp);
 
@@ -14,6 +18,24 @@ describe('index GET', function () {
       .end(function (err, res) {
         expect(err).to.be.null;
         expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('should render the content of index ejs', function (done) {
+    chai.request(server)
+      .get('/')
+      .end(function (err, res, body) {
+        // Render the view using ejs
+        let path = join(__dirname, '../views/index.ejs');
+        let data = { title: 'Jama Software Capstone' };
+        let rendHtml = ejs.compile(read(path, 'utf8'), {filename: path})(data);
+
+        // Response rendered html
+        let respHtml = res.text;
+
+        expect(err).to.be.null;
+        expect(respHtml).to.be.equal(rendHtml);
         done();
       });
   });
