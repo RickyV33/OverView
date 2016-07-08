5263747c7fae2b7c33e28ecba61e3079342e2c32
Ruben <rub3@pdx.edu>
2016-07-08 10:31:00 -0700

    Added title and items array to the projects object

--- routes/projects.js
+++ routes/projects.js
@@ -5,11 +5,17 @@ let router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   // Parse the returned JSon
-  let projectsList = [];
+  let projectsList = {
+    title: 'Project Array',
+    items: []
+  };
 
   req.session.projects.forEach(function (p) {
-    let item = { id: p.id, name: p.fields.name };
-    projectsList.push(item); // Push the current project
+    // id and name are required fields
+    if (p.id && p.fields.name) {
+      let item = {id: p.id, name: p.fields.name};
+      projectsList.items.push(item); // Push the current project
+    }
   });
 
   res.render('projects', { title: 'Jama Software Capstone', projects: projectsList });
