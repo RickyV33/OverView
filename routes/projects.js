f6676d97e84994254adbbcfd8adaa30b6402c2f0
Ruben Piatnitsky <rub3@pdx.edu>
2016-07-08 00:30:51 -0700

    Added project object parse file and tested the contents of it

--- routes/projects.js
+++ routes/projects.js
@@ -1,9 +1,12 @@
 let express = require('express');
+let projParse = require('../lib/projectsParse');
 let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('projects', { title: 'Jama Software Capstone', projects: req.session.projects });
+  // Parse the returned JSon
+  let projectsList = projParse(req.session.projects);
+  res.render('projects', { title: 'Jama Software Capstone', projects: projectsList });
 });
 
 module.exports = router;
