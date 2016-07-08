7fbb2dd655887edfd4704de9a9512f8613e16f84
Ruben Piatnitsky <rub3@pdx.edu>
2016-07-08 00:51:16 -0700

    Cleaned up the code to extract the project name and id

--- routes/projects.js
+++ routes/projects.js
@@ -1,11 +1,17 @@
 let express = require('express');
-let projParse = require('../lib/projectsParse');
+// let projParse = require('../lib/projectsParse');
 let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   // Parse the returned JSon
-  let projectsList = projParse(req.session.projects);
+  let projectsList = [];
+
+  req.session.projects.forEach(function (p) {
+    let item = { id: p.id, name: p.fields.name };
+    projectsList.push(item); // Push the current project
+  });
+
   res.render('projects', { title: 'Jama Software Capstone', projects: projectsList });
 });
 
