b12bca730927805380e7d052777e5b2531eb8449
ibilal <ibilal@pdx.edu>
2016-07-13 03:17:58 -0700

    Added a check for projects in projects.ejs before looping through array. Added test for projects route.

--- routes/projects.js
+++ routes/projects.js
@@ -4,7 +4,7 @@ let router = express.Router();
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.status(200);
-  res.render('projects', { title: 'Projects', projects: req.session.projects });
+  res.render('projects', { title: 'Projects', projects: req.session.projects ? req.session.projects : null});
 });
 
 module.exports = router;
--- test/routes/projects.js
+++ test/routes/projects.js
@@ -5,23 +5,44 @@ let chai = require('chai');
 let expect = chai.expect;
 let chaiHttp = require('chai-http');
 let server = require('../../app');
-// let ejs = require('ejs');
-// let read = require('fs').readFileSync;
-// let join = require('path').join;
+let ejs = require('ejs');
+let read = require('fs').readFileSync;
+let join = require('path').join;
 
 chai.use(chaiHttp);
 
 describe('projects GET', function () {
-  let testCases = [
-    {title: "Select a project: "}
-  ];
 
-  // Test #1: verify projects list view is rendered successfully with status code 200
+  // Test #1: verify projects list view is rendered successfully as is documented in views/projects.ejs
   it('should render successfully with status 200', function (done) {
-    chai.request(server).get('/projects').end(function (err, res) {
-      expect(err).to.be.null;
-      expect(res).to.have.status(200);
-      done();
-    });
+    let path, data, renderedView;
+    chai.request(server)
+        .post('/')
+        .send({teamName: 'sevensource', username: 'dummy', password: 'password'})
+        .end(function (err, res) {
+          if (err) {
+            console.log(err);
+          }
+          expect(res).to.have.status(200);
+          expect(res).to.redirect;
+          expect(res).to.have.property('text');
+          expect(res.charset).to.equal('utf-8');
+          path = join(__dirname, '../../views/projects.ejs');
+          data = { title: 'Projects', projects: null};
+          renderedView = ejs.compile(read(path, 'utf8'), {filename: path})(data);
+          expect(res.text).to.equal(renderedView);
+          expect(res.text).contains('</ul>');
+          expect(res.text).contains('Projects');
+          expect(res.text).contains('Click on a project below to view it\'s traceability graph:');
+          done();
+        });
   });
 });
+
+
+
+
+
+
+
+
