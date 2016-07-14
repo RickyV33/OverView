bedd81ab414707744f7f44cba1e236f78a570b6b
ibilal <ibilal@pdx.edu>
2016-07-14 12:52:25 -0700

    Removed explicitly setting response status to 200

--- routes/projects.js
+++ routes/projects.js
@@ -3,7 +3,6 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.status(200);
   res.render('projects', {title: 'Projects', projects: req.session.projects ? req.session.projects : null});
 });
 
