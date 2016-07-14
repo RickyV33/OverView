19ca512c5383f520e31e2e31c1bb8497a7c788b0
Ruben <rub3@pdx.edu>
2016-07-14 10:33:40 -0700

    Added endpoint testing to routes/projects.js

--- test/routes/projects.js
+++ test/routes/projects.js
@@ -1,13 +1,21 @@
 /* eslint-env mocha */
 'use strict';
+
 let chai = require('chai');
 let expect = chai.expect;
 let chaiHttp = require('chai-http');
-// let server = require('../../app.js');
+let server = require('../../app');
 
+// For the schema validation test
 let Ajv = require('ajv');
 let projectListSchema = require('../../schema/projectList');
 let projects = require('../../lib/projects');
+
+// For routes test
+let ejs = require('ejs');
+let read = require('fs').readFileSync;
+let join = require('path').join;
+
 chai.use(chaiHttp);
 let mockProjects = require('./mockProjects.json');
 
@@ -94,8 +102,7 @@ describe('Validation for parseProjectsList in lib/project.js', function () {
       {id: 234, name: 'Chatty'},
       {id: 345, name: 'IIBA BABOK'},
       {id: 456, name: 'Integrated System'}
-    ],
-    [
+    ], [
       {id: 345, name: 'IIBA BABOK'},
       {id: 456, name: 'Integrated System'}
     ]
@@ -137,10 +144,40 @@ describe('Validation for parseProjectsList in lib/project.js', function () {
     expect(parsed).to.deep.equal(filterJSON[1]);
     expect(result).to.be.true();
   });
+
   it('Should return an empty array of projects when passed a null', function () {
     // When projects are null
     let empty = [];
     let parsed = projects.parseProjectList(null);
     expect(parsed).to.deep.equal(empty);
+  });
+});
+
+describe('Validation for the Project Endpoint', function () {
+  it('should return a status of 200', function () {
+    chai.request(server)
+      .get('/projects')
+      .end(function (err, res) {
+        expect(err).to.be.null();
+        expect(res).to.have.status(200);
+      });
+  });
+
+  it('should render the contents of the projects endpoint', function (done) {
+    chai.request(server)
+      .get('/projects')
+      .end(function (err, res, body) {
+        // Render the view using ejs
+        let path = join(__dirname, '../../views/projects.ejs');
+        // This data below has to match the view data variables being passed by
+        // routes/projects.js
+        let data = { title: 'Jama Software Capstone' };
+        let rendHtml = ejs.compile(read(path, 'utf8'), {filename: path})(data);
+        let respHtml = res.text;   // Response rendered html
+
+        expect(err).to.be.null();
+        expect(respHtml).to.be.equal(rendHtml);
+        done();
+      });
   });
 });
