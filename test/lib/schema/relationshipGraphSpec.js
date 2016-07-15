45945ae78b64e2a46c3c99cff0eba70590e25893
ibilal <ibilal@pdx.edu>
2016-07-14 23:56:25 -0700

    Moved relationshipGraphSpec.json both in lib and test to uder a schema directory.

--- test/lib/schema/relationshipGraphSpec.js
+++ test/lib/schema/relationshipGraphSpec.js
@@ -5,7 +5,7 @@ let dirtyChai = require('dirty-chai');
 let chaiAsPromised = require('chai-as-promised');
 let Ajv = require('ajv');
 
-let schema = require('../../lib/relationshipGraphSpec');
+let schema = require('../../../lib/schema/relationshipGraphSpec');
 
 chai.use(dirtyChai);
 chai.use(chaiAsPromised);
