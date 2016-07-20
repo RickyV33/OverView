7609489c5d060cc17c31a524d021786f60b9d38a
ibilal <ibilal@pdx.edu>
2016-07-20 14:19:08 -0700

    Changed app.js to require hierarchy route. changed package.json to include hierarchy build. Added routes/hierarchy.js for item hierarchy endpoint. This branch contains code to get item hierarchy (for testing the view is rendered with live data) but will be removed and put into its own branch for story gatherItemHierarchy

--- routes/hierarchy.js
+++ routes/hierarchy.js
@@ -1,19 +1,53 @@
 let express = require('express');
+let hierarchy = require('../lib/hierarchy');
+
 let router = express.Router();
 
 let rootItemSelected = '';
-let itemHierarchy = {};
+let itemHierarchy = [];
+let children = [];
+let results = [];
 
 /* GET item hierarchy page */
-router.get('/', function(req, res, next){
-    // get a item hierarchy and make sure it's been validated for valid item hierarchy items.
-    itemHierarchy = getItemHierarchy();
-    res.render('hierarchy', {title: 'Please select a a root node, or click render graph to render from the project root: ',
-    itemHierarchy: itemHierarchy});
-});
+router.get('/', function(req, res, next) {
+    // These lines will replace line 14 with the explicit URL
+  // let url = 'http://' + req.session.username + ':' + req.session.password + '@' + req.session.teamname + '.jamacloud.com/rest/latest/';
+  hierarchy.getRootItems(33, 'http://dummy:password@sevensource.jamacloud.com/rest/latest/').then(function(rootItems){
+      itemHierarchy = hierarchy.parseRootItems(rootItems);
+      itemHierarchy.forEach(function(rootItem){
+          children = [];
+          hierarchy.getSubItems('http://dummy:password@sevensource.jamacloud.com/rest/latest/', rootItem.id).then(function(subItems){
+              children = hierarchy.parseSubRootItems(subItems);
+              children.forEach(function(child){
+                  rootItem.children.push(child);
+              });
+              rootItem.children.forEach(function(child){
+                  console.log('child in router: ;' + child.name);
+              });
+              results.push(rootItem);
+          });
+      });
 
-router.post('/', function(req, res, next){
-    rootItemSelected = res.selectedItem;
+
+      /*req.sesseion.itemHierarchy = itemHierarchy;
+      req.session.save(function (err) {
+          if (err) {
+              // TODO Session save Error message
+          }
+          else {
+            res.render('hierarchy', {
+              title: 'Select a Root Item (Optional) ',
+              itemHierarchy: req.session.itemHierarchy
+            });
+          }
+      });*/
+  });
+    console.log('results');
+    res.render('hierarchy', {
+        title: 'Select a Root Item (Optional) ',
+        itemHierarchy: results
+    });
+
 });
 
 module.exports = router;
---
+++ test/routes/hierarchy.js
@@ -0,0 +1,40 @@
+/* eslint-env mocha */
+'use strict';
+
+let chai = require('chai');
+let expect = chai.expect;
+let chaiHttp = require('chai-http');
+let server = require('../../app');
+
+let ejs = require('ejs');
+let read = require('fs').readFileSync;
+let join = require('path').join;
+
+chai.use(chaiHttp);
+
+describe('hierarchy', function () {
+    describe('get request', function () {
+        it('should contain a property called text', function (done) {
+            // This makes a server request to the route location '/'
+            chai.request(app)
+                .get('/')
+                .end(function (err, res) {
+                    if (err) {
+                        expect.fail();
+                    }
+                    // Render the view using ejs
+                    let path = join(__dirname, '../../views/hierarchy.ejs');
+                    let data = { title: 'Jama Software Capstone' };
+                    let rendHtml = ejs.compile(read(path, 'utf8'), {filename: path})(data);
+
+                    // Response rendered html
+                    let respHtml = res.text;
+
+                    expect(res).to.have.property('text');
+                    expect(err).to.be.null();
+                    expect(respHtml).to.be.equal(rendHtml);
+                    done();
+                });
+        });
+    });
+});
