0ad499d32a1b82048b33820a9db71bdca136b55c
ibilal <ibilal@pdx.edu>
2016-07-21 20:29:17 -0700

    Final version of hierarchy endpoint for this story. Testing to come

--- routes/hierarchy.js
+++ routes/hierarchy.js
@@ -1,5 +1,4 @@
 let express = require('express');
-let hierarchy = require('../lib/hierarchy');
 
 let router = express.Router();
 
@@ -7,29 +6,7 @@ let itemHierarchy = [];
 
 /* GET item hierarchy page */
 router.get('/', function (req, res, next) {
-  // Next two lines will replace line 13 with explicit url
-  // let url = 'http://' + req.session.username + ':' + req.session.password + '@' + req.session.teamname + '.jamacloud.com/rest/latest/';
-  // hierarchy.getItemHierarchy(req.session.username, req.session.password, req.session.teamName, 33).then(function(allItems){
-  hierarchy.getAllItems('dummy', 'password', 'sevensource', 33,
-    'http://dummy:password@sevensource.jamacloud.com/rest/latest/').then(function (allItems) {
-    // req.session.rootItems = parseItemHierarchy(allItems); to replace the line below
-      itemHierarchy = hierarchy.parseItemHierarchy(allItems);
-      res.render('hierarchy', {
-        title: 'Select a Root Item (Optional) ',
-        rootItems: itemHierarchy
-      });
-     /* req.session.save(function (err) {
-      if (err) {
-        // TODO Session save Error message
-      } else {
-        res.render('hierarchy', {
-          title: 'Select a Root Item (Optional) ',
-          // rootItems: req.session.rootItems to replace the line below
-          rootItems: rootItems
-        });
-      }
-    });*/
-    });
+  res.render('hierarchy', {title: 'Select a Root Item (Optional) ', itemHierarchy: itemHierarchy});
 });
 
 module.exports = router;
