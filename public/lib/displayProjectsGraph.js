eb50518a74d35c9b690ff069cbcb0162d062769a
Ruben Piatnitsky <rub3@pdx.edu>
2016-07-31 16:10:15 -0700

    Reorganized the update function for graph. Added a project node and styling. Added edges from project node and passed node. Added Comments and cleaned up code a bit

--- public/lib/displayProjectsGraph.js
+++ public/lib/displayProjectsGraph.js
@@ -1,5 +1,7 @@
-"use strict";
 let fileName = '../js/ssProject.json'; // '../js/sampleGraphData.json'
+
+// TODO: If a root id is passed, then we should add a relationship from the project node to the selected id node
+// TODO: Add the project name as the first node in the nodes list of JSON being given
 
 // get the data
 d3.json(fileName, function (error, graphData) {
@@ -15,6 +17,11 @@ d3.json(fileName, function (error, graphData) {
   // let nodes = {};
   let items = graphData.nodes;
   let itemRelations = graphData.edges;
+
+  // Create a project node and add it to the items list
+  let projectNode = { id: -1, name: graphData.name, image: '', type: -1 };
+  items.unshift(projectNode);
+  projectNode = items[0];
 
   // Append the SVG object to the body
   // Add a group element within it to encompass all the nodes - this fixes the chrome
@@ -43,102 +50,55 @@ d3.json(fileName, function (error, graphData) {
     .attr('d', 'M0,-5L10,0L0,5')
     .attr('class', 'arrow');
 
+  let force = d3.layout.force()
+    .size([width, height])
+    .linkDistance(100)  // sets the target distance between linked nodes to the specified value
+    .charge(-950)       // - value results in node repulsion, while + value results in node attraction
+    .friction(0.5);     // closely approximates velocity decay
+
   updateGraph(passedID);  // Render the graph
 
   /*
-   * Updates the graph
-   * @param rootId - This is the id of the element that is to be the root
+   * Updates the graph visuals
+   * @param rootId - This is the id of the element that is to be the root node coming off the project node
    */
   function updateGraph (rootId = -1) {
-    let defaultRootId = -1.1;
-    let defaultRootNodeType = -1.1;
-    let defaultRelLinkId = -1.2;
-    let defaultRelType = -1.2;
-
-    let itemsToRender = [{id: defaultRootId, name: graphData.name, type: defaultRootNodeType}];  // The list of nodes to render
-    let itemRelationsToRender = []; // The list of relationships to render
-    let projectNode;  // References the root node
-    let rootLink = {};     // references the relationship from project name to selected node
-    let linkedByIndex = [];      // Array with info as to what is connected to what
-
-    // Compute the distinct nodes from the edges.
-    for (let i = 0; i < items.length; i++) {
-      linkedByIndex[items[i].id + ',' + items[i].id] = 1;
-      items[i].downstream = []; // This will store the index for for relationships for each item *see downstreamCheck() below
-      items[i].downstream.noRelations = false;
-    }
-
-    // Check to see if the user passed in a node id
-    // If one is provided, then create a link from the project node and add a new
-    // object to be rendered. Otherwise create edges from project node to first level of nodes
-    if (rootId >= 0) {
-      console.log('Root ID: ' + rootId);
-      // Set the node source and target
-      /*itemRelations.forEach( relItem => {
-        if (relItem.source === rootId) {
-          console.log(relItem);
-          itemRelationsToRender.push(relItem);           // Save the relationship to render
-        }
-        // itemRelationsToRender.push(relItem); // Add the relationships to the relationship array
-        // relItem.value = +relItem.type;
-        // create relationship tuples for linkedByIndex array
-        // linkedByIndex[relItem.source.id + ',' + relItem.target.id] = 1;
-      });*/
-
-      // Go through each item relationship and find all of the nodes related to the passed node ID
-      /*itemRelationsToRender.forEach( relItem => {
-        let foundItem = items.filter( function (item) {
-          return item.id === relItem.target;
-        });
-
-        if(foundItem) {
-          itemsToRender.push(foundItem[0]);  // Save the node to render
-        }
-      });*/
-
-
-      // Get the item object
-      let foundItem = items.filter(function (item) {
-        return item.id === rootId;
+    // For each relationship, add the target to the source node
+    itemRelations.forEach(function (relItem) {
+      let srcNode = items.find(function (item) {
+        return item.id === relItem.source;
       });
 
-      if (foundItem.length > 0) {
-        projectNode = foundItem[0];  // Reference is make at function scope
-        itemsToRender.push(projectNode);  // Add the selected item to the render list
+      // Set the relationship source and target values
+      // Note - Use objects only and not just ids of the nodes
+      relItem.source = srcNode;
+      relItem.target = items.find(function (item) {
+        return item.id === relItem.target;
+      });
 
-        // Add an edge from project name to the selected root id provided
-        rootLink = {
-          id: defaultRelLinkId,
-          sourceNode: defaultRootId,
-          targetNode: rootId,
-          type: defaultRelType
-        };
-
-        itemRelations.unshift(rootLink);  // Add the node and edge to the front of the render lists
-        console.log(itemRelations[0]);
-        itemRelationsToRender.push(itemRelations[0]); // Take the edge just created and put it in the render list
+      // Check if downstream items array exists
+      if (typeof srcNode.downstream === 'undefined') {
+        srcNode.downstream = [];
       }
-    } else {
-      // TODO: Add the nodes that do not have parents to the project node
-      // project name is the root node, so add edges to all of the first level nodes
-      /*itemsToRender.forEach( function (item) {
-        let newEdge = { id: 999, source: defaultRootId, target: item.id, type: defaultRelType};
-        itemRelationsToRender.push(newEdge);
-      });*/
+      srcNode.downstream.push(relItem.target);  // Add the target ID to list of downstream items
+
+      // relItem.value = +relItem.type;
+    });
+
+    // Checks to see if a node id was passed
+    if (rootId !== -1) {
+      // Get the root item from the items list
+      let rootItem = items.find(function (item) {
+        return item.id === rootId;
+      });
+      // Add an edge from the project node to the passed node
+      itemRelations.unshift({id: 0, source: projectNode, target: rootItem, type: -1});
     }
 
-    console.log('Items list');
-    console.log(itemsToRender);
-    console.log('Relations list');
-    console.log(itemRelationsToRender);
-
-    let force = d3.layout.force()
-      .nodes(d3.values(itemsToRender))
-      .links(itemRelationsToRender)
-      .size([width, height])
-      .linkDistance(100) // sets the target distance between linked nodes to the specified value
-      .charge(-950)      // - value results in node repulsion, while + value results in node attraction
-      .friction(0.5)     // closely approximates velocity decay
+    // Set the force nodes, edges and start the graph
+    force
+      .nodes(graphData.nodes)
+      .links(graphData.edges)
       .on('tick', tick)
       .start();
 
@@ -147,22 +107,30 @@ d3.json(fileName, function (error, graphData) {
       .data(force.links())
       .enter().append('svg:path')
       .attr('class', thisPath => {
-        return 'link ' + (thisPath.type === 8 ? 'link-dash' : ''); // Checks the path type and formats accordingly
+        let result = 'link';
+        // Check the type and add a style according to type
+        result = (thisPath.type === -1) ? result + ' link-dot' : result;
+        result = (thisPath.type === 8) ? ' link-dash' : result;
+
+        return result;
       })
       .attr('marker-end', 'url(#end)');
 
-    // define each node property
+    // ============ Node Properties Definition ===========
     let node = svg.selectAll('.node')
       .data(force.nodes())
-      .enter().append('g')
+      .enter()
+      .append('g')
       .attr('class', function (thisNode) {
-        // Check to see if the node is the project root node
+        // Add projectRoot class if the node is the project node
         return thisNode.type === -1 ? 'node projectRoot' : 'node';
       })
       .call(force.drag)
       .on('click', nodeClick);
 
-    // White rectangle behind text
+    projectNode.fixed = true;  // Set the project Node to be fixed and not moving
+
+    // White rectangle behind text configuration
     node.append('rect')
       .attr('x', -8)
       .attr('y', -9)
@@ -170,14 +138,14 @@ d3.json(fileName, function (error, graphData) {
       .attr('width', 120)
       .style('opacity', 0.80);
 
-    // Circle at node behind icon
+    // Circle at node behind icon configuration
     node.append('circle')
       .attr('x', '-14px')
       .attr('y', '-14px')
       .attr('r', 13)
       .style('opacity', 0.80);
 
-    // Image in the node circle
+    // Image in the node circle configuration
     node.append('image')
       .attr('xlink:href', function (n) {
         return n.image;
@@ -187,13 +155,13 @@ d3.json(fileName, function (error, graphData) {
       .attr('height', '18px')
       .attr('width', '18px');
 
-    // add a title for hovering that shows full filename
+    // Add Text for hovering that shows full filename
     node.append('svg:title')
       .text(function (d) {
         return d.name;
       });
 
-    // add the text
+    // Add the name of the node as text
     node.append('text')
       .attr('x', 18)
       .attr('dy', '.35em')
@@ -202,7 +170,11 @@ d3.json(fileName, function (error, graphData) {
         return d.name.length > 18 ? d.name.substring(0, 15) + '...' : d.name;
       });
 
-    // ============= add the curvy lines ==============
+    // ============= Node Path Definitions ==============
+    /*
+     * For every shift of the graph, this gets called.
+     * It updates the location of the nodes and edges.
+     */
     function tick (e) {
       path.attr('d', function (d) {
         let dx = d.target.x - d.source.x;
@@ -213,64 +185,52 @@ d3.json(fileName, function (error, graphData) {
           dr + ',' + dr + ' 0 0,1 ' + d.target.x + ',' + d.target.y;
       });
 
+      // Move the edge depending on node location
       node.attr('transform', function (d) {
         return 'translate(' + d.x + ',' + d.y + ')';
       });
 
       let k = 10 * e.alpha; // For the node offset
 
-      // Push sources up and targets down to form a weak tree-like structure.
+      // This section pushes sources up and targets down to form a weak tree-like structure.
       path.each(function (d) {
         d.source.x -= k;
         d.target.x += k;
-      })
-        .attr('x1', function (d) {
-          return d.source.x;
-        })
-        .attr('y1', function (d) {
-          return d.source.y;
-        })
-        .attr('x2', function (d) {
-          return d.target.x;
-        })
-        .attr('y2', function (d) {
-          return d.target.y;
-        });
+      }).attr('x1', function (d) { return d.source.x; })
+        .attr('y1', function (d) { return d.source.y; })
+        .attr('x2', function (d) { return d.target.x; })
+        .attr('y2', function (d) { return d.target.y; });
 
-      node.attr('cx', function (d) {
-        return 5 * d.x;
-      })
-        .attr('cy', function (d) {
-          return d.y;
-        });
-    }
-
-    // Gets the item with id
-    function getItemWithId (id) {
-      items.filter( function (item) {
-        return item.id === id;
-      });
+      // This sets the node position
+      node.attr('cx', function (d) { return 5 * d.x; })
+          .attr('cy', function (d) { return d.y; });
     }
 
     /*
      * Traverse through child nodes recursively
+     * @param: d - is the selected node
      */
-
     function downstreamCheck (d) {
-      if (d.downstream.length === 0 && d.downstream.noRelations === false) {
-        // build downstream if you don't have one already and don't have the noRelationships flag set.
-        graphData.edges.forEach(function (relItem, index) {
-          if (d.id === relItem.source) {
-            d.downstream.push(relItem);
+      if (typeof d.downstream !== 'undefined') {
+        if (d.downstream.length === 0 && d.downstream.noRelations === false) {
+          // build downstream if you don't have one already and don't have the noRelationships flag set.
+          graphData.edges.forEach(function (relItem, index) {
+            if (d.id === relItem.source) {
+              d.downstream.push(relItem);
+            }
+          });
+          // This will be called if we have NO RELATIONSHIPS. So we're setting that flag.
+          if (d.downstream.length === 0) {
+            d.downstream.noRelations = true;
           }
-        });
-        // This will be called if we have NO RELATIONSHIPS. So we're setting that flag.
-        if (d.downstream.length === 0) {
-          d.downstream.noRelations = true;
         }
       }
     }
 
+    /*
+     * Shows the Nodes downstream by recursing through them
+     * @param: d - is the selected node
+     */
     function showNodesDownstream (d) {
       // let downStreamArray = [];
       //
@@ -281,9 +241,11 @@ d3.json(fileName, function (error, graphData) {
       // });
 
       node.style('opacity', function (curNode) {
-        for (let i = 0; i < d.downstream.length; i++) {
-          if (d.downstream[i].target === curNode.id) {
-            curNode.isHighlighted = true;
+        if (typeof d.downstream !== 'undefined') {
+          for (let i = 0; i < d.downstream.length; i++) {
+            if (d.downstream[i].target === curNode.id) {
+              curNode.isHighlighted = true;
+            }
           }
         }
         return curNode.isHighlighted ? 1 : 0.1;
@@ -294,14 +256,16 @@ d3.json(fileName, function (error, graphData) {
       });
     }
 
-    // ============ Toggle highlighting children on single click ===========
-    function nodeClick () {
+    // ======= Click Events Below =======
+    /*
+     * Node click event toggles highlighting children on single click
+     */
+    function nodeClick (clickedNode) {
       // Reduce the opacity of all but its neighbors
-      let d = d3.select(this).node().__data__;
-      downstreamCheck(d);
-      d.isSelected = true;
-      d.isHighlighted = true;
-      showNodesDownstream(d);  // Run algorithm for showing all of the downstream items
+      downstreamCheck(clickedNode);
+      clickedNode.isSelected = true;
+      clickedNode.isHighlighted = true;
+      showNodesDownstream(clickedNode);  // Run algorithm for showing all of the downstream items
     }
   }
 });
