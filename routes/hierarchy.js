61942c37ca819212defc4c8a4fc0a3ac3903ef35
ibilal <ibilal@pdx.edu>
2016-08-01 16:03:57 -0700

    Updated hierarchy endpoint tests - now no longer accessing jama's servers

--- routes/hierarchy.js
+++ routes/hierarchy.js
@@ -1,48 +1,11 @@
-/* let express = require('express');
-
+let express = require('express');
 let router = express.Router();
-
 let itemHierarchy = [];
 
-/* GET item hierarchy page
+// GET item hierarchy page
 router.get('/', function (req, res, next) {
   res.render('hierarchy', {title: 'Select a Root Item (Optional) ', itemHierarchy: itemHierarchy});
 });
 
-module.exports = router;*/
-
-let express = require('express');
-let hierarchy = require('../lib/hierarchy');
-
-let router = express.Router();
-let itemHierarchy = [];
-
-/* GET item hierarchy page */
-router.get('/', function (req, res, next) {
-  // Next two lines will replace line 13 with explicit url
-  // let url = 'http://' + req.session.username + ':' + req.session.password + '@' + req.session.teamname + '.jamacloud.com/rest/latest/';
-  // hierarchy.getItemHierarchy(req.session.username, req.session.password, req.session.teamName, 33).then(function(allItems){
-  hierarchy.getAllItems('dummy', 'password', 'sevensource', 33,
-      'http://dummy:password@sevensource.jamacloud.com/rest/latest/').then(function (allItems) {
-    // req.session.rootItems = parseItemHierarchy(allItems); to replace the line below
-        itemHierarchy = hierarchy.parseItemHierarchy(allItems);
-        console.log('bler' + hierarchy.renderItemHierarchy(itemHierarchy));
-        res.render('hierarchy', {
-          title: 'Select a Root Item (Optional) ',
-          itemHierarchy: itemHierarchy
-        });
-    /* req.session.save(function (err) {
-     if (err) {
-     // TODO Session save Error message
-     } else {
-     res.render('hierarchy', {
-     title: 'Select a Root Item (Optional) ',
-     // rootItems: req.session.rootItems to replace the line below
-     rootItems: rootItems
-     });
-     }
-     });*/
-      });
-});
-
 module.exports = router;
+
--- test/routes/hierarchy.js
+++ test/routes/hierarchy.js
@@ -10,6 +10,10 @@ let express = require('express');
 let request = require('supertest');
 let app = express();
 let router = express.Router();
+let ejs = require('ejs');
+let read = require('fs').readFileSync;
+let join = require('path').join;
+let path = join(__dirname, '../../views/hierarchy.ejs');
 
 chai.use(dirtyChai);
 chai.use(chaiPromise);
@@ -17,37 +21,74 @@ chai.use(chaiPromise);
 
 function initializeRoute (object) {
   router.get('/hierarchy', function (req, res) {
-    res.status(200).send({title: 'Select a root item (Optional)',
-      itemHierarchy: [{name: 'oneName', children: [{name: 'childName', children: []}]}]});
+   // let rendHtml = ejs.compile(read(path, 'utf8'), {filename: path})(object);
+    res.status(200).send(ejs.compile(read(path, 'utf8'), {filename: path})(object));
   });
 }
 
+let hierarchyTestCases = [
+  {title: 'Empty item hierarchy tree (no root items)',
+    itemHierarchy: []},
+
+  {title: 'Single root item with no children',
+    itemHierarchy: [{name: 'Root Item', children: []}]},
+
+  {title: 'Single root item with one child',
+    itemHierarchy: [{name: 'Root Item', children: [{name: 'single child', children: []}]}]},
+
+  {title: 'Single root item with mulitple children',
+    itemHierarchy: [{name: 'Root Item', children:
+    [{name: 'child one', children: []}, {name: 'child two', children: []}]}]},
+
+  {title: 'Single root item with one nested child',
+    itemHierarchy: [{name: 'Root Item', children:
+    [{name: 'single child',
+      children: [{name: 'nested child', children: []}]}]}]},
+
+  {title: 'Mulitple root items with no children',
+    itemHierarchy: [{name: 'Root Item 1', children: []}, {name: 'Root item 2', children: []}]},
+
+  {title: 'Multiple items with children',
+    itemHierarchy: [{name: 'Root Item 1', children: [{name: 'Sub root item 1', children: []}]},
+      {name: 'Root Item 2', children: [{name: 'Sub root item 2', children: []}]}]},
+
+  {title: 'Multiple root items with multiple children',
+    itemHierarchy:
+      [{name: 'Root item 1',
+        children: [{name: 'Root 1 Child 1', children: []}, {name: 'Root 1 Child 1', children: []}]},
+      {name: 'Root item 2',
+        children: [{name: 'Root 2 Child 1', children: []}, {name: 'Root 2 Child 2', children: []}]}]},
+
+  {title: 'Multiple root items with one nested child',
+    itemHierarchy:
+      [{name: 'Root item 1',
+        children: [{name: 'Root 1 Child 1', children: []}, {name: 'Root 1 Child 1',
+          children: [{name: 'Nested child of root 1', children: []}]}]},
+      {name: 'Root item 2',
+        children: [{name: 'Root 2 Child 1', children: []}, {name: 'Root 2 Child 2',
+          children: [{name: 'Nested child of root 2', children: []}]}]}]}
+];
+
 describe('hierarchy', function () {
   describe('GET /hierarchy', function () {
-    initializeRoute({});
-    app.use(router);
-    request(app)
-      .get('/hierarchy')
-      .end(function (err, res) {
-        if (err) {
-          throw (err);
-        } else {
-          expect(res.statusCode).to.equal(200);
-          expect(res.body).to.deep.equal({
-            "itemHierarchy": [
-              {
-                "children": [
-                  {
-                    "children": [],
-                    "name": "childName"
-                  }
-                ],
-                "name": "oneName"
-              }
-            ],
-            "title": "Select a root item (Optional)"
+    it('Should render item hierarchy view according to item hierarchy test case', function (done) {
+      hierarchyTestCases.forEach(function (item) {
+        initializeRoute(item);
+        app.use(router);
+        request(app)
+          .get('/hierarchy')
+          .end(function (err, res) {
+            if (err) {
+              return done(err);
+            } else {
+              let rendHtml = ejs.compile(read(path, 'utf8'), {filename: path})(item);
+              expect(res).to.have.property('text');
+              expect(res.status).to.equal(200);
+              expect(res.text).to.be.equal(rendHtml);
+              done();
+            }
           });
-        }
       });
+    });
   });
 });
