e542327cfdd7a5fd1d502b21b96fc1525705aa23
Ricardo Valencia <RickyV33@users.noreply.github.com>
2016-08-02 19:49:31 -0700

    add TODO on logout button

--- src/graph.js
+++ src/graph.js
@@ -1,5 +1,7 @@
 /* eslint-env browser */
 
+// TODO: Hook this up to the log out button on the graph view. It is currently not hooked up to anything, 
+// but functions properly
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('logOut').addEventListener('click', () => {
     document.location.href = '/logout';
