b54a63b25b5624c9eb18761bf37f5cda1b6e3961
Ricardo Valencia <RickyV33@users.noreply.github.com>
2016-08-02 19:51:54 -0700

    lint yelled at me (again)

--- src/graph.js
+++ src/graph.js
@@ -1,6 +1,6 @@
 /* eslint-env browser */
 
-// TODO: Hook this up to the log out button on the graph view. It is currently not hooked up to anything, 
+// TODO: Hook this up to the log out button on the graph view. It is currently not hooked up to anything,
 // but functions properly
 document.addEventListener('DOMContentLoaded', () => {
   document.getElementById('logOut').addEventListener('click', () => {
