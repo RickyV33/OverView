b6944e8a3fa443c61b769239aff3b44178a3f990
Ricky Valencia <3d.rickyV@gmail.com>
2016-08-02 19:36:12 -0700

    remove ternary null

--- routes/projects.js
+++ routes/projects.js
@@ -3,7 +3,7 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-  res.render('projects', {title: 'Projects', projects: req.session.projects ? null: null});
+  res.render('projects', {title: 'Projects', projects: req.session.projects ? req.session.projects : null});
 });
 
 module.exports = router;
