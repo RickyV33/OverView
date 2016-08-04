c946eb90058865aa0f1dfdc7cc7aa8b1aa328236
Chance Snow <git@chancesnow.me>
2016-08-04 09:41:22 -0700

    Add REST get handler for graph data

--- routes/graph.js
+++ routes/graph.js
@@ -1,13 +1,25 @@
 let express = require('express');
 let router = express.Router();
 
+let Graph = require('./../lib/graph');
+
 /* GET Project */
 router.get('/', function (req, res) {
-  res.render('graph', {
-    title: 'Jama Software Capstone',
-    subtitle: 'Graph',
-    projects: req.session.projects ? req.session.projects : null
-  });
+  if (req.query.project) {
+    if (req.session.username && req.session.password && req.session.teamName) {
+      let graph = new Graph(req.query.project, 'http://' + req.session.username + ':' +
+        req.session.password + '@' + req.session.teamName + '.jamacloud.com/rest/latest/')
+        .ready.then(() => {
+          res.json(graph.toJson());
+        });
+    }
+  } else {
+    res.render('graph', {
+      title: 'Jama Software Capstone',
+      subtitle: 'Graph',
+      projects: req.session.projects ? req.session.projects : null
+    });
+  }
 });
 
 module.exports = router;
