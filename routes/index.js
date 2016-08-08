e4ac1c80f68ccbe1456de2b2ee2cc91a3fa51f80
Ricardo Valencia <RickyV33@users.noreply.github.com>
2016-08-08 15:59:51 -0700

    remove empty lines

--- routes/index.js
+++ routes/index.js
@@ -5,9 +5,7 @@ let router = express.Router();
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
-
   res.render('index', {title: 'JamaTrace', teamName: req.session.teamName,  error: 'false'});
-
 });
 
 router.post('/', function (req, res, next) {
