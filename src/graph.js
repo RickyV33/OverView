b232a646e60833576715b6b8002b65446145fb08
Ricky Valencia <3d.rickyV@gmail.com>
2016-08-08 00:21:38 -0700

    hook up item hierarchy, and reorganize file

--- src/graph.js
+++ src/graph.js
@@ -2,14 +2,55 @@
 
 import './lib/displayProjectsGraph';
 
+
+let projects = document.querySelector('#projects');
+let hierarchy = document.querySelector('#hierarchy');
+
 // TODO: Hook this up to the log out button on the graph view. It is currently not hooked up to anything,
 // but functions properly
 document.addEventListener('DOMContentLoaded', () => {
+  // let ejs = require('ejs');
+  // let read = require('fs').readFileSync;
+  // let join = require('path').join;
+  
   document.getElementById('logOut').addEventListener('click', () => {
     document.location.href = '/logout';
   });
+  
+  querySelectorAll('#projects a').forEach(projectAnchor => {
+    projectAnchor.addEventListener('click', event => {
+      let selectedProject = event.target.getAttribute('data-id');
+      console.log(selectedProject);
+      // Hides projects
+      // Shows hierarchy
+      // document.body.style.cursor='wait';
+      // toggleProjects();
+      // toggleHierarchy();
+      setTimeout(() => {document.body.style.cursor='default';toggleProjects();toggleHierarchy();}, 2000);
+
+      getHierarchy(selectedProject).then(hierarchyPayload => {
+        updateHierarchy(hierarchyPayload).then(updatedHtml => {
+          let div = document.createElement('div');
+          div.innerHTML = updatedHtml;
+          let newDiv = div.firstChild;
+          let childList = Array.from(newDiv.childNodes);
+          hierarchy.innerHTML = '';
+          childList.forEach(child => {
+            hierarchy.appendChild(child);
+          });
+          bindRenderButton();
+        });
+      });
+    });
+  });
 });
 
+function bindRenderButton() {
+  document.getElementById('renderButton').addEventListener('click', event => {
+    toggleHierarchy();
+    document.querySelector('svg').style.display = 'block';
+  });
+}
 /**
  * Query the page for a set of elements given a CSS selector.
  *
@@ -20,44 +61,23 @@ function querySelectorAll (selector) {
   return Array.prototype.slice.call(document.querySelectorAll(selector));
 }
 
-let projects = document.querySelector('#projects');
-let hierarchy = document.querySelector('#hierarchy');
-
 function toggleHierarchy () {
   if (hierarchy.classList.contains('hidden')) {
+    console.log('SHOW');
     hierarchy.classList.remove('hidden');
   } else {
+    console.log('Hidden');
     hierarchy.classList.add('hidden');
   }
 }
 
-function hideProjects () {
-  projects.classList.add('hidden');
+function toggleProjects () {
+  if (projects.classList.contains('hidden')) {
+    projects.classList.remove('hidden');
+  } else {
+    projects.classList.add('hidden');
+  }
 }
-
-document.addEventListener('DOMContentLoaded', () => {
-  querySelectorAll('#projects a').forEach(projectAnchor => {
-    projectAnchor.addEventListener('click', event => {
-      let selectedProject = event.target.getAttribute('data-id');
-
-      hideProjects();
-      toggleHierarchy();
-
-      getHierarchy(selectedProject).then(hierarchy => {
-        console.log(hierarchy);
-      });
-      getGraph(selectedProject).then(graph => {
-        console.log(graph);
-      });
-      // TODO: change query parameter (dependent on hierarchy endpoint story)
-    });
-  });
-
-  document.getElementById('renderButton').addEventListener('click', event => {
-    toggleHierarchy();
-    document.querySelector('svg').style.display = 'block';
-  });
-});
 
 /**
  * Makes an AJAX request to the provided endpoint for the item hierarchy tree.
@@ -82,6 +102,26 @@ function getHierarchy (projectId) {
     httpRequest.open('GET', '/hierarchy?project=' + projectId);
     httpRequest.send();
   });
+}
+
+function updateHierarchy (payload) {
+  return new Promise((resolve, reject) => {
+    let httpRequest = new XMLHttpRequest();
+    httpRequest.onreadystatechange = () => {
+      if (httpRequest.readyState === XMLHttpRequest.DONE) {
+        if (httpRequest.status === 200) {
+          resolve(httpRequest.responseText);
+        } else {
+          console.log('There was a problem requesting to the hierarchy endpoint.');
+          reject({status: httpRequest.status, response: httpRequest.responseText});
+        }
+      }
+    };
+    httpRequest.open('POST', '/graph/update-hierarchy');
+    httpRequest.setRequestHeader('Content-Type', 'application/json');
+    httpRequest.send(JSON.stringify(payload));
+  });
+
 }
 
 /**
