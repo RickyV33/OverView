e0750fa23cc631cbc53ce8caf508175ea629e339
Ricky Valencia <3d.rickyV@gmail.com>
2016-08-08 23:15:02 -0700

    remove post on graph endpoint

--- routes/graph.js
+++ routes/graph.js
@@ -4,35 +4,22 @@ let router = express.Router();
 let Graph = require('./../lib/graph');
 
 router.get('/', function (req, res) {
-  if (req.query.project) {
-    if (req.session.username && req.session.password && req.session.teamName) {
-      let graph = new Graph(req.query.project, 'http://' + req.session.username + ':' +
-        req.session.password + '@' + req.session.teamName + '.jamacloud.com/rest/latest/');
-      graph.buildGraph(() => {
-        res.status(200).json(graph.toJson());
-      });
-    }
-  } else {
     res.render('graph', {
       title: 'JamaTrace',
       subtitle: 'Graph',
       projects: req.session.projects ? req.session.projects : null,
       itemHierarchy: null
     });
-  }
 });
 
-router.post('/update-hierarchy', function (req, res) {
-  console.log('post');
-  res.render('partials/hierarchy', { itemHierarchy: req.body }, (err, html) => {
-    console.log(err);
-    console.log(html);
-    if (err){
-      console.error(err);
-    } else {
-      res.status(200).send(html).end();
-    }
-  });
+router.get('/getGraphData/:id', function (req, res) {
+  if (req.session.username && req.session.password && req.session.teamName) {
+    let graph = new Graph(req.params.id, 'http://' + req.session.username + ':' +
+      req.session.password + '@' + req.session.teamName + '.jamacloud.com/rest/latest/');
+    graph.buildGraph(() => {
+      res.status(200).json(graph.toJson());
+    });
+  }
 });
 
 module.exports = router;
