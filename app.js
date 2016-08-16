6766a0ab1c152cd9b7320692c4d05e1e552cfed2
ibilal <ibilal@pdx.edu>
2016-08-15 20:06:08 -0700

    Removed line from PR comments

--- app.js
+++ app.js
@@ -39,8 +39,6 @@ app.use((req, res, next) => {
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/projects', projects);
 app.use('/logout', logout);
