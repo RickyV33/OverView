141b4ddaa79dd561306743147286dd95a4f68db5
Ricky Valencia <3d.rickyV@gmail.com>
2016-08-15 21:58:19 -0700

    comment src/lib/project.js

--- src/lib/project.js
+++ src/lib/project.js
@@ -4,6 +4,12 @@ import { querySelectorAll } from '../graph';
 
 export let selectedProject;
 
+/**
+ * Listens for mouse clicks on the project list and sets the selectedProject variable to that project's ID. It returns 
+ * a promise that signals that the project has been selected.
+ *
+ * @returns {Promise} signals that that the project has been selected
+ */
 export function buildProjectAnchors () {
   return new Promise((resolve) => {
     querySelectorAll('#projects a').forEach(projectAnchor => {
